81b9c5953e84f7060813ab08237c7ea9747db5a7
Alecx Moritz <alecxmoritz7@gmail.com>
2019-03-26 12:03:23 -0400

    finished code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const styles = {
+    backgroundColor : 'purple'
+};
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      title : 'React Is Cool'
     }
   }
 
+  handleClick = () => {
+    this.setState({
+      title : 'React is Awesome!'
+    })
+  }
+
   render() {
+    const paragraph = <p>React React React React React React React React</p>
+
     return (
-      <div>
-          Test
+      <div style={ styles } className="div">
+          <h2 className="app">{ this.state.title }</h2>
+          <hr /> 
+          <button onClick={ this.handleClick }>Click Me!</button>
+          { paragraph }
       </div>
     );
   }
